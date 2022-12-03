c799110a2e9292df2061bb8426f4c2f53f8d0d48
Mac15001900 <mac15001900@gmail.com>
2022-12-03 13:21:14 +0100

    Day 3 refactor

--- day3.hs
+++ day3.hs
@@ -3,22 +3,17 @@ import Data.List
 import Data.Char
 
 priority :: Char -> Int
-priority a 
-    | isUpper a = (ord a) - 38
-    | otherwise = (ord a) - 96
+priority a = (ord a) - (if isUpper a then 38 else 96)
 
 splitHalfs :: String -> (String, String)
 splitHalfs as = (take x as, drop x as) where x = (length as) `div` 2
 
 findCommon :: Eq a => [a] -> [a] -> a
-findCommon [] _ = error "No common part"
-findCommon (x:xs) ys = if exists (==x) ys then x else findCommon xs ys
+findCommon xs ys = filter ((flip elem) ys) xs |> head
 
 part1 :: [String] -> Int
 part1 lines = map splitHalfs lines |> tupleMap findCommon |> map priority |> sum
 
-part2 lines = part2' lines 0
-
-part2' :: [String] -> Int -> Int
-part2' [] n         = n
-part2' ((a:as):bs:cs:xs) n = if (a `elem` bs) && (a `elem` cs) then part2' xs (n+(priority a)) else part2' (as:bs:cs:xs) n
+part2 :: [String] -> Int
+part2 []                = 0
+part2 ((a:as):bs:cs:xs) = if (a `elem` bs) && (a `elem` cs) then (priority a) + (part2 xs) else part2 (as:bs:cs:xs)
