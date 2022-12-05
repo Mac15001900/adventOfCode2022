3063a2d0d7fed291ba214b2ed96522f238ddacbe
Mac15001900 <mac15001900@gmail.com>
2022-12-05 12:44:00 +0100

    Day 4 refactor

--- day4.hs
+++ day4.hs
@@ -4,14 +4,14 @@ import Data.List
 parseLine :: String -> ((Int,Int), (Int,Int))
 parseLine line = ((a,b), (x,y)) where [[a,b],[x,y]] = splitOn ',' line |> map (splitOn '-') |> map2 read
 
-isFullyContined :: ((Int,Int), (Int,Int)) -> Bool
-isFullyContined ((a,b), (x,y)) = (a>=x && b <=y) || (x>=a && y<=b)
+isFullyContained :: ((Int,Int), (Int,Int)) -> Bool
+isFullyContained ((a,b), (x,y)) = (a>=x && b <=y) || (x>=a && y<=b)
 
 isOverlap :: ((Int,Int), (Int,Int)) -> Bool
 isOverlap ((a,b), (x,y)) = not $ y<a || x>b
 
 part1 :: [String] -> Int
-part1 = count (isFullyContined . parseLine)
+part1 = count (isFullyContained . parseLine)
 
 part2 :: [String] -> Int
 part2 = count (isOverlap . parseLine)
