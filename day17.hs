b300eac80333b21aa112dcf1395ee087579255a8
Mac15001900 <mac15001900@gmail.com>
2022-12-19 18:18:41 +0100

    Day 17 clean up

--- day17.hs
+++ day17.hs
@@ -2,7 +2,6 @@ import MyUtils
 import Data.List
 import Data.Maybe
 import qualified Data.HashSet as Set
-import Debug.Trace
 
 type Pos = (Int, Int)
 type Shape = ([Pos]) --List of coordinates relative to the bottom-left corner (which might not be included itself, like with the + shape)
@@ -23,7 +22,6 @@ addPos :: Pos -> Pos -> Pos
 addPos (x1,y1) (x2,y2) = (x1+x2,y1+y2)
 
 fallDown :: Board -> Stream -> Shape -> Pos -> (Board, Stream, Int)
---fallDown b _ shape pos | trace (showBoard (Set.union (Set.fromList (map (addPos pos) shape)) b) 10 0) False = error "xD"
 fallDown b ([],sb) shape pos = fallDown b (sb,sb) shape pos
 fallDown b (s:ss,sb) shape pos = if fits b shape shiftedV then fallDown b (ss,sb) shape shiftedV else (Set.union (Set.fromList inserted) b, (ss,sb), map snd inserted |> maximum) where
     tryHShift = addPos pos (streamShift s)
@@ -48,15 +46,11 @@ part1 :: [String] -> Int
 part1 input = runFor input 2022
 
 findCycle :: State -> [(Int, Int, Int, Board)] -> Int -> (Int, Int, Int) --Start state, list of base cycle endings, shapes placed (cycle start, cycle length, cycle height)
---findCycle s h n | (n `mod` 10000==0) && (traceShow n False) = error ""
 findCycle (b, st,       [], m) h n = findCycle (b, st, shapes, m) h n
-findCycle (b, (st,stb), sh, m) h n | length st < 10 = case getCycle newEntry h of
+findCycle (b, (st,stb), sh, m) h n | length st < length stb = case getCycle newEntry h of
         Nothing -> findCycle (b, (st++stb,stb), sh, m) (newEntry:h) n
         Just c -> c
-    where
-    newEntry = (n, m, length sh, combinations [0..6] [m-40..m] |> filter (flip Set.member b) |> map (mapSnd (subtract m)) |> Set.fromList)
-    --newEntry = traceShowId (n, m, length sh, combinations [0..6] [m-40..m] |> filter (flip Set.member b) |> map (mapSnd (subtract m)) |> Set.fromList)
-    --newEntry = trace (showBoard b (m+1) (m-20)) (n, m, length sh, combinations [0..6] [m-40..m] |> filter (flip Set.member b) |> map (mapSnd (subtract m)) |> Set.fromList)
+    where newEntry = (n, m, length sh, combinations [0..6] [m-40..m] |> filter (flip Set.member b) |> map (mapSnd (subtract m)) |> Set.fromList)
 findCycle (b, st, (sh:shs), m) h n = findCycle (b', st', shs, max m m') h (n+1) where (b',st',m') = fallDown b st sh (2, m+4)
 
 getCycle :: (Int, Int, Int, Board) -> [(Int, Int, Int, Board)] -> Maybe (Int, Int, Int)
@@ -67,7 +61,6 @@ getCycle (n,m,sh,b) h = case find (\(n',m',sh',b')-> b==b' && sh==sh') h of
 part2 :: [String] -> Int
 part2 input = ((target-cStart) `div` cLength) * cHeight + extraHeight where
     target = 1000000000000 :: Int
-    --target = 2022 :: Int
     start = (Set.fromList (map (\x-> (x,0)) [0..6]), (head input, head input), shapes, 0)
     (cStart, cLength, cHeight) = findCycle start [] 0
     extraHeight = runFor input (cStart + ((target-cStart) `mod` cLength))
@@ -82,64 +75,3 @@ showBoard b n f | n<f       = ""
 runTest :: Int -> IO()
 runTest n = putStrLn (showBoard b m (max (m-2500) 0)) where
     (b,m) = runAll (Set.fromList (map (\x-> (x,0)) [0..6]), (head test, head test), shapes, 0) n
-
-{-
-####
-
-.#.
-###
-.#.
-
-..#
-..#
-###
-
-#
-#
-#
-#
-
-##
-##
--}
-
-
-{-
-part2 :: [String] -> Int
-part2 input = traceShow [m1, m2, m2', m3] $ m1 + (((target `div` baseCycle)-1) * (m2'-m1)) + (m3-m1) where
-    target = 1000000000000 :: Int
-    start@(b0, st, sh, m0) = (Set.fromList (map (\x-> (x,0)) [0..6]), (head input, head input), shapes, 0)
-    baseCycle = (length $ snd st) * 5
-    (b1,m1)   = runAll start            baseCycle
-    (b2,m2)   = runAll (b1, st, sh, m1) baseCycle
-    (b2',m2') = runAll start (baseCycle*2)
-    (b3,m3)   = runAll start ((target `mod` baseCycle) + baseCycle)
-
---A dumb attempt that does not work
---1595560400379 is too low
---1596472103819 is too high
-brokenPart2 :: [String] -> Int
-brokenPart2 input = (target `div` cycle) * cycleHeight + (traceShowId (runFor input (target `mod` cycle))) where
-    target      = 1000000000000 :: Int
-    cycle       = traceShowId $ head input |> length |> (*5)
-    cycleHeight = traceShowId $ runFor input cycle
-
-part2 :: [String] -> Int
-part2 input = (target `div` cycle) * cycleHeight + (runFor input (target `mod` cycle)) where
-    target = 1000000000000 :: Int
-    start = (Set.fromList (map (\x-> (x,0)) [0..6]), (head input, head input), shapes, 0)
-    (cycle, cycleHeight) = findCycle start
-
-findCycle :: State -> (Int, Int) --Starting state, (cycle length, cycle height)
---findCycle (_,_,_,m) | traceShow m False = error "xD"
-findCycle (b,_,_,m) | trace ((showBoard b m (m-10))++"-------") False = error "xD"
-findCycle s@(b, (st,sb), sh, m) = if valid b' m' then (baseCycle, m') else mapFst (+baseCycle) (findCycle (b', (st,sb), sh, m')) where
-    baseCycle = 5 * (length sb)
-    (b', m') = runAll s baseCycle
-
-valid :: Board -> Int -> Bool
---valid b m = [0..2] |> any (\x-> Set.member (x,m) b) |> not
-valid b m = (Set.member (1,m) b) && (any (\x-> Set.member (x,m) b) [3..6])
-
-
--}
